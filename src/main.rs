794b3c1317508410cd59a226d7639b52c81c3b83
Sean McPherson <git@seanmcp.com>
2020-03-25 20:40:56 -0400

    Add stdin, exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+
+    let mut name = String::new();
+
+    io::stdin().read_line(&mut name).unwrap();
+
+    match name.trim().parse() {
+        Ok(value) => {
+            name = value;
+        },
+        Err(error) => {
+            println!("Error: {}", error);
+            process::exit(1);
+        }
+    }
+
+    println!("Hello, {}!", name);
 }
